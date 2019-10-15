0170b2e6b8d70e882c37bbb50afeb4feb560e4ab
nguyenquangtruong-c0919h1 <quangtruong0412@gmail.com>
2019-10-15 09:33:25 +0700

    firt commit

---
+++ src/LapTrinhJava/KhaiBaoVaSuDungBien.java
@@ -0,0 +1,20 @@
+package NgonNguLapTrinhJava;
+
+import java.util.concurrent.CountDownLatch;
+
+public class KhaiBaoVaSuDungBien {
+    public static void main(String[] args){
+        int i = 10;
+        float f= 20.5f;
+        double d= 20.5;
+        boolean b= true;
+        char c= 'a';
+        String s= "Ha noi";
+        System.out.println(i + "\n");
+        System.out.println(f + "\n");
+        System.out.println(d + "\n");
+        System.out.println(b + "\n");
+        System.out.println(c + "\n");
+        System.out.println(s + "\n");
+    }
+}
